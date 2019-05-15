87e5827e18ccc4cf479da21d06071938ea953384
Andrew <swabe@live.co.za>
2019-05-15 17:32:19 +0200

    Updated database project

--- Accounting.Database/dbo/Stored Procedures/spSaveExpenditure.sql
+++ Accounting.Database/dbo/Stored Procedures/spSaveExpenditure.sql
@@ -6,6 +6,10 @@ BEGIN
 		insert into Expenditure(TransactionId)
 			select  t.TransactionId 
 			from Transactions t with (nolock)
+				inner join TransactionTypes tt
+				on t.TransactionTypeId=tt.TransactionTypeId
 			where TransactionTimestamp BETWEEN @startDate AND  @endDate 
-			and NOT EXISTS(select TransactionId from Expenditure)
+			and AccountTypeId=1
+			and TransactionType='Withdraw'
+			and NOT EXISTS(select e.TransactionId from Expenditure e where t.TransactionId=e.TransactionId)
 END
