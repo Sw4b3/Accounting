e160c65456b5d615e7dfbe7e8a7511135b2ec30e
Andrew <swabe@live.co.za>
2019-05-28 19:52:13 +0200

    Updated database project

--- Accounting.Database/dbo/Stored Procedures/spGetAnalyticsStatistics.sql
+++ Accounting.Database/dbo/Stored Procedures/spGetAnalyticsStatistics.sql
@@ -4,18 +4,33 @@ CREATE PROCEDURE [dbo].[spGetAnalyticsStatistics]
 AS
 BEGIN
 		select 'Average Per Day (Overall)' as StatisticName, avg(Amount) as Stat
-			from (select avg(Amount) as Amount
+			from (select sum(Amount) as Amount
 			from Transactions 
 			where TransactionTypeId=2
 			group by TransactionTimestamp) as Ag
 
 		union all 
 
-		select 'Average Per Day (Month)' as StatisticName, avg(Amount) as Stat
-			from (select avg(Amount) as Amount
+		select 'Average Per Day (Current Month)' as StatisticName, avg(Amount) as Stat
+			from (select sum(Amount) as Amount
 			from Transactions 
 			where TransactionTypeId=2
 			and TransactionTimestamp between @startDate and @endDate and AccountTypeId=1
 			group by TransactionTimestamp) as Ag
+
+		union all 
+
+		select 'Average Per Spending (Overall)' as StatisticName, avg(Amount) as Stat
+			from Transactions 
+			where TransactionTypeId=2
+			
+
+		union all 
+
+		select 'Average Per Spending (Current Month)' as StatisticName, avg(Amount) as Stat
+			from Transactions 
+			where TransactionTypeId=2
+			and TransactionTimestamp between @startDate and @endDate and AccountTypeId=1
+	
 
 END
--- Accounting.Database/dbo/Stored Procedures/spGetExpenditureRuleOverview.sql
+++ Accounting.Database/dbo/Stored Procedures/spGetExpenditureRuleOverview.sql
@@ -3,6 +3,7 @@ CREATE PROCEDURE [dbo].[spGetExpenditureRuleOverview]
 AS
 BEGIN		
 		select sum(t.Amount) ExpenditureTotal, er.ExpenditureDesc,er.ExpenditureLimit 
+		into #TempTable
 		from Expenditure e
 			inner join Transactions t
 			on e.TransactionId=t.TransactionId
@@ -11,4 +12,11 @@ BEGIN
 			inner join ExpenditureTypes et
 			on et.ExpenditureTypeId=er.ExpenditureTypeId
 			group by  e.ExpenditureRuleId,er.ExpenditureDesc, et.ExpenditureTypeId,er.ExpenditureLimit
+
+		select  ExpenditureTotal, ExpenditureDesc, ExpenditureLimit 
+		from #TempTable
+		union all 
+		select  sum(ExpenditureTotal), 'Total' ExpenditureDesc, sum(ExpenditureLimit) 
+		from #TempTable
+		where ExpenditureDesc != 'Other'
 END
