bb352e2c921cad5a70c23ca094d16b0111242bdd
Andrew <swabe@live.co.za>
2019-06-20 21:35:14 +0200

    Improved logic of reverting an import file

--- Accounting.Database/dbo/Stored Procedures/spRevertImportFile.sql
+++ Accounting.Database/dbo/Stored Procedures/spRevertImportFile.sql
@@ -2,9 +2,17 @@
 CREATE PROCEDURE [dbo].[spRevertImportFile]
 AS
 BEGIN
-	declare @currentImportDate datetime = (select top 1 ImportDate from ProcessedImportFiles order by ImportDate desc);
+	declare @currentImportDate datetime, @currentBalance decimal(10,2), @previousPending decimal(10,2)
+	
+	set @currentImportDate  = (select top 1 ImportDate from ProcessedImportFiles order by ImportDate desc)
+	set @previousPending=(select CurrentBalance-AvailableBalance from Accounts where AccountId = 1 )
 
 	delete Transactions where ImportDate=@currentImportDate
 	delete ProcessedImportFiles where ImportDate=@currentImportDate
+
+	set @currentBalance  = (select top 1 Balance from Transactions where AccountTypeId=1 order by TransactionId desc)
+	
+	UPDATE Accounts SET CurrentBalance = @currentBalance WHERE AccountId = 1
+	UPDATE Accounts SET AvailableBalance = CurrentBalance-@previousPending WHERE AccountId = 1;
 
 END
