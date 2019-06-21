075dccaa65fb84b99ba4b550bd2b754fb8566864
Andrew <swabe@live.co.za>
2019-06-21 20:12:20 +0200

    Hotfix: fixed available balance calculation for all account types

--- Accounting.Database/dbo/Stored Procedures/spSaveTransaction.sql
+++ Accounting.Database/dbo/Stored Procedures/spSaveTransaction.sql
@@ -6,16 +6,16 @@ BEGIN
 	declare @currentBalance decimal(10,2),  @availableBalance decimal(10,2),@credit decimal(10,2),@debit decimal(10,2)
 	set @currentBalance=(select CurrentBalance from Accounts where AccountId = @accountTypeId )
 	set @availableBalance=(select AvailableBalance from Accounts where AccountId = @accountTypeId )
-	set @credit=(select sum(Amount) from TransactionsStaging where AccountTypeId = 1 and TransactionTypeId=1)
-	set @debit=(select sum(Amount) from TransactionsStaging where AccountTypeId = 1 and TransactionTypeId=2)
+	set @credit=(select sum(Amount) from TransactionsStaging where AccountTypeId = @accountTypeId and TransactionTypeId=1)
+	set @debit=(select sum(Amount) from TransactionsStaging where AccountTypeId = @accountTypeId and TransactionTypeId=2)
 
 		if @availableBalance != 0.00
 			begin
 			UPDATE Accounts SET CurrentBalance = @balance WHERE AccountId = @accountTypeId;
 			if @credit is not null 
-				UPDATE Accounts SET AvailableBalance = CurrentBalance-(@debit-@credit) where AccountId = 1;
+				UPDATE Accounts SET AvailableBalance = CurrentBalance-(@debit-@credit) where AccountId = @accountTypeId;
 			else
-				UPDATE Accounts SET AvailableBalance = CurrentBalance-@debit where AccountId = 1;
+				UPDATE Accounts SET AvailableBalance = CurrentBalance-@debit where AccountId = @accountTypeId;
 			end
 		else
 			begin
