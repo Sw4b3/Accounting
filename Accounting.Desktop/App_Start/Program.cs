316dab9d936e0bff17704db6987d69381ea3839b
Andrew <swabe@live.co.za>
2019-11-02 22:59:50 +0200

    Added ThreadException event to prevent unhandled exception

--- Accounting.Desktop/App_Start/Program.cs
+++ Accounting.Desktop/App_Start/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +17,13 @@ namespace Accounting.Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
             Application.Run(new MainApplication());
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show("An error has occurred:\n \n  " + e.Exception.Message, "Error", MessageBoxButtons.OK);
         }
     }
 }
