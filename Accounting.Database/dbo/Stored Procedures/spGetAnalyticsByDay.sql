f1a0f743f0eaffe3119e17c70cf381b14325760a
Andrew <swabe@live.co.za>
2019-11-18 22:31:52 +0200

    Improved spGetAnalyticsByDay logic

--- Accounting.Database/dbo/Stored Procedures/spGetAnalyticsByDay.sql
+++ Accounting.Database/dbo/Stored Procedures/spGetAnalyticsByDay.sql
@@ -3,10 +3,19 @@ CREATE PROCEDURE [dbo].[spGetAnalyticsByDay]
 @startDate datetime, @endDate datetime
 AS
 BEGIN
-		select TransactionTimestamp, sum(Amount) as Amount
-		from Transactions 
-		where TransactionTypeId=2 and TransactionTimestamp between @startDate and @endDate and AccountTypeId=1
-		and Description not in ('WEEKLY', 'WEEK','FOOD', 'SAVINGS' ) 
-		group by TransactionTimestamp  
+		;with dates(date) as
+		(select dateadd(dd, 0, datediff(dd, 0, @startDate)) as date
+		union all
+		select dateadd(day, 1, date) as date
+		from dates
+		where date <=  getdate())
 
+		select date as TransactionTimestamp, isNull(sum(Amount), 0) as Amount
+		from Dates d
+			left join Transactions t
+			on d.date = t.TransactionTimestamp 
+			 and AccountTypeId=1
+			and TransactionTypeId=2 
+		where  date between @startDate and @endDate
+		group by d.date  
 END
